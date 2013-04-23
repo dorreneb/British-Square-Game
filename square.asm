6b757801b4ad0034e6bfb4095ddc9e74d7c6a9b8
Dorrene Brown <deb3323@rit.edu>
2013-04-23 00:10:04 -0400

    prints out the title of the game

--- square.asm
+++ square.asm
@@ -4,16 +4,43 @@
 	.data
 	.align	0
 
+# The header for the square program
+intro_string:
+	.ascii  "\n****************************\n"
+	.ascii	  "**     British Square     **\n"
+	.asciiz   "****************************\n\n"
+
 #
 # CONSTANTS
 #
+
+# tells syscall what to do
+WRITE_STRING = 4
+EXIT_PROGRAM = 10
 
 #
 # CODE
 #
 	.text
 	.align	2
-	.globl main
+	j	main			# Start program main method
 
+#
+# Prints out a string, the pointer to which is saved in $a0.
+# Arguments:	$a0:	A pointer to the string that should be printed.
+#
+print_string:
+	li	$v0, WRITE_STRING	# Tells system to write
+	syscall				# Writes string
+	jr	$ra			# Return
+
+#
+# Main method. Runs program.
+#
 main:
-	jr $ra
+	la	$a0, intro_string
+	jal	print_string
+
+exit_program:
+	li	$v0, EXIT_PROGRAM	# Tells program to exit
+	syscall				# Exit program
