17e834ad35bbbe23e60070ebf1a9d4c5ddca7d8b
Dorrene Brown <deb3323@rit.edu>
2013-04-23 02:57:18 -0400

    Have potential strings saved. Prints out top and bottom row.

--- square.asm
+++ square.asm
@@ -4,11 +4,39 @@
 	.data
 	.align	0
 
+#
+# STRINGS
+#
+
 # The header for the square program
 intro_string:
 	.ascii  "\n****************************\n"
 	.ascii	  "**     British Square     **\n"
 	.asciiz   "****************************\n\n"
+# The first and last row of the board
+board_border:
+	.asciiz	"***********************\n"
+# Delineates tops and bottoms of cells
+row_border:
+	.asciiz	"*+---+---+---+---+---+*\n"
+# The left edge of the board
+cell_start:
+	.asciiz "*|"
+# Seperates east and west cells
+cell_seperator:
+	.asciiz	"|"
+# Right edge of the board
+cell_end:
+	.asciiz	"|*"
+# Proper amount of whitespace for top half of empty cells
+cell_blank:
+	.asciiz "   "
+# For filled cell rows marked by player 1
+x_fill_row:
+	.asciiz "XXX"
+# For filled cell rows marked by player 2
+o_fill_row:
+	.asciiz "OOO"
 
 #
 # CONSTANTS
@@ -35,11 +63,33 @@ print_string:
 	jr	$ra			# Return
 
 #
+# Prints board in accordance to the spec located at
+# http://www.cs.rit.edu/~vcss345/project/123/proj123.html
+#
+
+print_board:
+	addi	$sp, $sp, -4		# Make room on stack to save ra
+	sw	$ra, 0($sp)		# Save return location
+
+	la	$a0, board_border	# Print top border row
+	jal	print_string
+
+
+	la	$a0, board_border	# Print bottom border row
+	jal	print_string
+
+	lw	$ra, 0($sp)		# Restore return location
+	addi	$sp, $sp, 4		# Restore stack
+	jr	$ra			# Return
+
+#
 # Main method. Runs program.
 #
 main:
-	la	$a0, intro_string
+	la	$a0, intro_string	# Loads and prints intro string
 	jal	print_string
+
+	jal	print_board		# Print the board
 
 exit_program:
 	li	$v0, EXIT_PROGRAM	# Tells program to exit
