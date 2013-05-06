106e5b91adb5b25ee8ba5e9114449180777606b9
Dorrene Brown <deb3323@rit.edu>
2013-05-06 00:45:38 -0400

    Top half of each cell prints

--- square.asm
+++ square.asm
@@ -5,6 +5,16 @@
 	.align	0
 
 #
+# PLAY INFO
+#
+
+# 25-element integer array that keeps track of stone placement
+# -1 = clear, 0 = X claim, 1 = O claim
+board:
+	.space 100
+
+
+#
 # STRINGS
 #
 
@@ -107,6 +117,8 @@ print_int:
 #	$t2	Used to count index into row
 #	$t3	Row index comparator. Always equal to CELLS_PER_ROW.
 #	$t4	Used to store '9' so we know whether to print single digit or double digit whitespace 
+#	$t5	Used to store what cell we are currently printing
+#	$t6	Location in array to see board data
 #
 
 print_board:
@@ -117,6 +129,7 @@ print_board:
 	addi	$t1, $0, NUM_ROWS			# Save number of rows to print
 	addi	$t3, $0, CELLS_PER_ROW		# Save number of cells per row
 	addi	$t4, $0, 9				# Used to see if we should print one space or two spaces in row
+	la	$t6, board				# Load board into a register
 
 	la	$a0, board_border			# Print top table row
 	jal	print_string
@@ -134,13 +147,31 @@ print_row_top:					# Print each row
 print_cell_top:
 	beq	$t2, $t3, print_row_top_end		# If CELLS_PER_ROW is met finish row
 	
+	lw	$a0, 0($t6)				# Get contents of board array
+	blt	$a0, $0, print_top_whitespace
+	beq	$a0, $0, print_top_x
+	bgt	$a0, $0, print_top_o
+
+print_top_whitespace:
 	la	$a0, cell_blank			# Print whitespace
+	j	print_top_cell_content
+	
+print_top_x:
+	la	$a0, x_fill_row			# Print "XXX"
+	j	print_top_cell_content
+
+print_top_o:
+	la	$a0, o_fill_row			# Print "OOO"
+	j	print_top_cell_content
+
+print_top_cell_content:
 	jal	print_string
 
 	la	$a0, cell_seperator			# Print cell seperator
 	jal	print_string
 
 	addi	$t2, $t2, 1				# Increment cells in row printed
+	addi	$t6, $t6, 4				# Increment index of board array
 	j	print_cell_top			# Print another cell
 
 print_row_top_end:
@@ -234,6 +265,16 @@ print_prompt_done:
 #	$s3	Used to flip turn counter
 #	$s4	If input equals this, skip turn
 #	$s5	If input equals this, quit
+#	$s6	The address of the cell being affected by the current turn
+#	$s7	The max number of cells (NUM_ROWS * CELLS_PER_ROW)
+#
+# Temporary Variables (don't care about these, SUPER temporary, documented for future reference):
+#	$t0	Loop index when initializing board array
+#	$t1	Board address when initializing board array
+#	$t2	Default value for board initialization (-1)
+#	$t3	Stores location in array to store player turn input
+#	$t4	Used to calculate array locs
+#	$t5	Stores what's in $t3 to make sure things dont get overwritten
 #
 main:
 	add	$s0, $0, $0
@@ -243,6 +284,22 @@ main:
 	addi	$s4, $0, -1
 	addi	$s5, $0, -2
 
+	la	$t0, NUM_ROWS			# Get total number of cells in $s7
+	la	$t1, CELLS_PER_ROW
+	mul	$s7, $t0, $t1
+
+	add	$t0, $0, $0			# Index counter for initializing board
+	la	$t1, board			# The board
+	addi	$t2, $0, -1			# Default number for board -> -1 = unclaimed spot
+
+initialize_board:
+	beq	$t0, $s7, intro		# If the array is fully initialized, play game
+	sw	$t2, 0($t1)			# Put -1 in index in array
+	addi	$t0, $t0, 1			# Increment array
+	addi	$t1, $t1, 4			# Go to next index in array
+	j	initialize_board		# Initialize next index in array
+
+intro:
 	la	$a0, intro_string		# Loads and prints intro string
 	jal	print_string
 
@@ -260,6 +317,16 @@ play:
 
 	beq	$v0, $s4, turn_over		# If input == -1 skip turn
 	beq	$v0, $s5, exit_program	# If input == -2 quit
+	bge	$v0, $s7, turn_over		# If input > the max square, skip turn
+	blt	$v0, $s5, turn_over		# If input < quit, it's invalid, skip turn
+	
+	la	$t3, board			# Get index of square to place
+	addi 	$t4, $0, 4			# Multiplier to get to proper array index
+	mul	$t4, $t4, $v0			# Get relative location of index
+	add	$t3, $t3, $t4			# $t3 now has location in array to put things
+	lw	$t5, 0($t3)			# Check if a player has put something in the cell specified
+	bge	$t5, $0, turn_over		# If $t5 > 0, do not persist input
+	sw	$s2, 0($t3)			# Put player turn into the array
 
 
 turn_over:
